56b2223b60c9d285d48347dbfdf735984b8d2168
Greg Price <gnprice@gmail.com>
2019-01-16 15:31:14 -0500

    pattern-matching: Use labeled, non-loop, blocks for easier hygiene.
    We've been using `continue` to break out of a non-match, like this:
    
      do {
        // ...
        if (!/* match */) continue;
        // ...
        /* body */
      } while (0);
    
    This can work fine, but would require adding some complexity because
    any `continue` or `break` statement inside the body, if it doesn't
    refer to an explicit label, requires rewriting to avoid having the
    new `do`-loop capture it.
    
    If instead we use `break LABEL`, then we can have the outer block be
    *not a loop*:
    
      _label: {
        // ...
        if (!/* match */) break _label;
        // ...
        /* body */
      }
    
    Now an unlabelled `break` or `continue` is unaffected, so the problem
    is reduced to generating a unique label to use -- which is a general
    problem we have general infrastructure for, in `scope.generateUid`.
    So do that.

--- packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/scoping/exec.js
+++ packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/scoping/exec.js
@@ -53,32 +53,58 @@
 })();
 
 
-/* TODO broken
+let log;
 (() => {
+  log = [];
   do {
     case (42) {
       when v -> {
+        log.push("when1");
         if (v) { continue }
-        expect(true).toBe(false);
+        log.push("when2");
       }
     }
-    expect(true).toBe(false);
+    log.push("do2");
   } while (0);
+  log.push("end");
 })();
+expect(log).toEqual(["when1", "end"]);
+
+
+let s;
+(() => {
+  s = "";
+  for (const i of [1, 2, 3, 4, 5]) {
+    s += `(${i}`;
+    case (i) {
+      when 2 -> continue;
+      when 4 -> break;
+    }
+    s += `${i})`;
+  }
+  s += ".";
+})();
+expect(s).toEqual("(11)(2(33)(4."); // no 5
 
 
 (() => {
-  do {
-    case (42) {
-      when v -> {
-        break;  // TODO test distinctly from `continue`
-        expect(true).toBe(false);
-      }
+  s = "";
+  for (const i of [1, 2, 3, 4, 5, 6]) {
+    s += `(${i}`;
+    case (i) {
+      // None of these affect the outer loop...
+      when 1 -> do { continue } while (0)
+      when 2 -> for (const x of [1]) { continue; }
+      when 3 -> switch(i) { default: continue; } // ... except this.
+      when 4 -> do { break } while (0)
+      when 5 -> for (const x of [1]) { break; }
+      when 6 -> switch(i) { default: break; }
     }
-    expect(true).toBe(false);
-  } while (0);
+    s += `${i})`;
+  }
+  s += ".";
 })();
-*/
+expect(s).toEqual("(11)(22)(3(44)(55)(66).");
 
 
 case ({ x: 1 }) {
